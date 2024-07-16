e439cc2d5073487ce62291d13f6016027b01a24a
qabriely <gsonatore.0132@unimar.edu.ve>
2024-07-15 23:04:34 -0400

    Prueba 3

---
+++ BancAmigaProyect.java
@@ -0,0 +1,14 @@
+
+public class BancAmigaProyect {
+
+    
+    public static void main(String[] args) {
+                    System.out.println("Inicio");
+                    System.out.println("Inicio 1 y 2");
+
+    }
+    
+}
+
+
+
